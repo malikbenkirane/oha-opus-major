6c253b21fdd9d083b52e3ad9706b13be51e9ed67
Malik Benkirane <mbenkirane@internetworks.blog>
2026-01-14 11:51:55 +0100

    Implement game design

---
+++ internal/domain/player/player.go
@@ -0,0 +1,17 @@
+package player
+
+import "github.com/malikbenkirane/oha-opus-major/internal/domain/weapon"
+
+// Player represents a 2‑D game entity with a position, orientation, and
+// the ability to fire weapons.
+type Data struct {
+	Position    Vector2         // (x, y) coordinates in world space
+	Orientation float64         // angle in radians, where 0 points to the right
+	CanFire     bool            // true if the player is currently able to fire
+	Weapons     []weapon.Weapon // Weapons holds the character's equipped weapons.
+	Health      float64         // Health is a percentage between 0 and 1.
+}
+
+type Vector2 struct {
+	X, Y float32
+}
---
+++ internal/domain/weapon/weapon.go
@@ -0,0 +1,6 @@
+package weapon
+
+type Weapon struct {
+	Name   string  // weapon identifier
+	Damage float64 // fixed damage applied on hit
+}
